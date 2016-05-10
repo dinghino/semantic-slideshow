b6888af250ba054efff744b24e3599b590e936c9
Daniele Ding <dinghino@gmail.com>
2016-05-10 18:58:40 +0200

    [REF/FIX] 1of 2 ~ main.js
    reorganize code
    update loaders to met new shortcuts and functionalities
    add APP global config object to toggle debuggers (see next commit)
    syntax fap

--- js/main.js
+++ js/main.js
@@ -1,68 +1,81 @@
-    $(document).ready(function () {
-      var $slides_loader
+var $slides_loader;
 
-      // callback for the success of load 'config.html'
-      // will activate the submit button, allowing the loading of slideshows
-      function _enableSlideLoader () {
-        $slides_loader.on('click', function (e) {
-          e.preventDefault();
+// callback for the success of load 'config.html'
+// will activate the submit button, allowing the loading of slideshows
+function _enableSlideLoader () {
+  $slides_loader.on('click', function (e) {
+    e.preventDefault();
 
-          var folder      = $('input[name="folderName"]').val(),
-              hash        = $('input[name="pageHash"]').val(),
-              counter     = $('input[name="counterToggle"]').is(':checked'),
-              controls    = $('input[name="controlsToggle"]').is(':checked');
+    var folder      = $('input[name="folderName"]').val(),
+        hash        = $('input[name="pageHash"]').val(),
+        counter     = $('input[name="counterToggle"]').is(':checked'),
+        controls    = $('input[name="controlsToggle"]').is(':checked'),
+        loadScript  = $('input[name="loadCustomScript"]').is(':checked');
 
-          // set the config object to load the slides
-          var config = {
-            folder: folder,
-            hash: hash,
-            showButtons: controls,
-            showCounter: counter
+    // set the config object to load the slides
+    var config = {
+      getEvents: loadScript,
+      folder: folder,
+      hash: hash,
+      showButtons: controls,
+      showCounter: counter
+    };
+
+    /** calback functions for App.validateSlideshowFolder */
+    function validateSuccess () { App.loadSlideshow(config) };
+    function validateFailure () {
+      var message = $('[data-role="loadingErrorBox"]'),
+          icon = $(message).children('.icon');
+
+      /** show error message! if already on screen then 'tada!' it */
+      if ($(message).hasClass('hidden')) {
+        $(message).transition({
+          animation: 'fade up',
+          onComplete: function () {
+            $(icon).transition('tada')
           }
-
-          function validateSuccess () { App.loadSlideshow(config) };
-          function validateFailure () {
-            var message = $('[data-role="loadingErrorBox"]'),
-                icon = $(message).children('.icon');
-
-            $(message).transition({
-              animation: 'fade up',
-              onComplete: function () {
-                $(icon).transition('tada')
-              }
-            })
-          };
-
-          App.validateSlideshowFolder(folder, validateSuccess, validateFailure);
-        });
+        })
+      } else {
+        $(message).transition('tada')
       }
+    };
 
-      function _loadApp () {
-        // fetch the UI and load it into the page
-        $('[data-role="ui"]').load("app/ui.html", function () {
-          // fetch the config file
-          $.getScript('app/config.js', function () {
-            // fetch app.js
-            $.getScript('js/app.js', function () {
-            // initialize the app
-            App.init();
-            // enable log for App.state changes
-            // used for debugging App.setState() functionality
-            // TODO: remove once satisfied
-            App.verbose = false
-            })
-          });
-        });
-      }
-      // load the configuration interface into the page
-      $( '[data-role="slides"]').load("app/config.html", function () {
-        // get the element for the load button
-        $slides_loader = $('#load_slideshow_btn');
-        // initialization for loading screen
-        $('.ui.checkbox').checkbox()
-        $('[data-role="configSettingsAccordion"]').accordion()
-        _enableSlideLoader()
-        // load the rest of the UI and 
-        _loadApp()
-      });
+    /** validation for @var folder. if success start loading slideshow */
+    App.validateSlideshowFolder(folder, validateSuccess, validateFailure);
+  });
+}
+
+function _loadApp () {
+  // fetch the UI and load it into the page
+  $('[data-role="slidesInterface"]').load("app/ui.html", function () {
+    // fetch the config file
+    $.getScript('app/config.js', function () {
+      // fetch app.js
+      $.getScript('js/app.js', function () {
+      
+      // initialize the app
+      var config = { verbose: true }
+
+      setTimeout(App.init, 10, config);
+      })
     });
+  });
+};
+
+
+$(document).ready(function () {
+  /** load the configuration interface into the page */
+  $( '[data-role="slides"]').load("app/config.html", function () {
+
+    /** get the element for the load button */
+    $slides_loader = $('#load_slideshow_btn');
+
+    /** initialization for loading screen */
+    $('.ui.checkbox').checkbox()
+    $('[data-role="configSettingsAccordion"]').accordion()
+    _enableSlideLoader()
+
+    /** load the rest of the UI and start initializing the application */
+    _loadApp()
+  });
+});
