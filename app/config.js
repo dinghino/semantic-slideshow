a88dc182a193910ca3636c484aec724d34e87df2
Daniele Ding <dinghino@gmail.com>
2016-05-10 18:57:06 +0200

    [FIX] UI fixes, integration and fappening

--- app/config.js
+++ app/config.js
@@ -1,23 +1,25 @@
 // selectors for DOM elements
-$helpIcon = $('[data-role="helpIcon"]'),
-$counter = $('[data-role="counter"]'),
-$authorAccordion = $('[data-role="authorAccordion"]'),
-$slideContainer = $('[data-role="slides"]');
-$dimmer = $('[data-role="dimmer"]')
-$button = {
-  // wrapper for slide movement buttons
-  wrapper: $('[data-role="buttons"]'),
-  // slide movement
-  first: $('[data-role="btnFirst"]'),
-  prev: $('[data-role="btnPrev"]'),
-  black: $('[data-role="blackScreen"]'),
-  next: $('[data-role="btnNext"]'),
-  last: $('[data-role="btnLast"]'),
-  // ui toggler
-  toggle: $('[data-role="toggle"]')
-};
-about = {
-  authorEmail: $('[data-role="authorEmail"]'),
-  authorGitHub: $('[data-role="authorGitHub"]'),
-  appVersion: $('[data-role="appVersion"]')
-};
+$helpIcon         = $('[data-role="helpIcon"]'),
+$slidesHelpPopup  = $('[data-role="helpPopup"]'),
+$counter          = $('[data-role="counter"]'),
+$authorAccordion  = $('[data-role="authorAccordion"]'),
+$slideContainer   = $('[data-role="slides"]');
+$dimmer           = $('[data-role="dimmer"]')
+
+$button           = {};
+/** wrapper for slide movement buttons */
+$button.wrapper   = $('[data-role="buttons"]');
+/** slide movement */
+$button.first     = $('[data-role="btnFirst"]');
+$button.prev      = $('[data-role="btnPrev"]');
+$button.black     = $('[data-role="blackScreen"]');
+$button.next      = $('[data-role="btnNext"]');
+$button.last      = $('[data-role="btnLast"]');
+/** ui toggler */
+$button.toggle    = $('[data-role="toggle"]')
+
+/** about spaces in the UI */
+about             = {};
+about.authorEmail = $('[data-role="authorEmail"]');
+about.authorGitHub= $('[data-role="authorGitHub"]');
+about.appVersion  = $('[data-role="appVersion"]');
